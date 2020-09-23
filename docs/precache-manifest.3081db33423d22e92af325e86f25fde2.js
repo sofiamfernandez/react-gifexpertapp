60e902c86d7337013b499ed77e7ab5c49ae3b6ea
Sofía M. Fernández <sofia.m.fernandez36@gmail.com>
2020-09-23 19:31:40 +0200

    docs agregado

---
+++ docs/precache-manifest.3081db33423d22e92af325e86f25fde2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfd67d1b907263ac35fe07e2eb25f563",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2dc65c9275cee38866d5",
+    "url": "/static/css/main.c6ebe2f9.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dc65c9275cee38866d5",
+    "url": "/static/js/main.d21de6a9.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
